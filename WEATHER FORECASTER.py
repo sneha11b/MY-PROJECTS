b30feb2e3db6b13c0a96e53259e82404e2eb200c
sneha11b <113909738+sneha11b@users.noreply.github.com>
2023-08-13 15:51:03 +0530

    Update WEATHER FORECASTER.py

--- WEATHER FORECASTER.py
+++ WEATHER FORECASTER.py
@@ -1,25 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# 
 # # WEATHER FORECASTER
-# 
-
-# In[1]:
-
 
 pip install requests
-
-
-# In[2]:
-
-
 import requests
-
-
-# In[3]:
-
-
 print("\t\tWelcome to the Weather Forecaster")
 print("Just enter the city you want the weather report")
 city_name=input("Enter the name of the city:")
@@ -34,56 +16,10 @@ def Gen_report(C):
 Gen_report(city_name)
 
 
-# # WTTR 
-# stands for "Weather in Terminal, Right Now." It is a command-line utility and a simple yet powerful tool for retrieving weather information directly from your terminal or command prompt. 
-# WTTR provides weather forecasts and current conditions using data from various weather sources.
-# 
-# By accessing WTTR in your terminal, you can quickly obtain weather information for a specified location or your current location. It offers a concise and easily readable 
-# presentation of weather data, including temperature, humidity, wind speed, and conditions like rain or snow. 
+# # WTTR stands for "Weather in Terminal, Right Now." It is a command-line utility and a simple yet powerful tool for retrieving weather information directly from your terminal or command prompt. 
+# WTTR provides weather forecasts and current conditions using data from various weather sources. 
+# By accessing WTTR in your terminal, you can quickly obtain weather information for a specified location or your current location. It offers a concise and easily readable presentation of weather data, including temperature, humidity, wind speed, and conditions like rain or snow. 
 # Additionally, WTTR supports various features such as displaying weather as ASCII art, providing short-term and long-term forecasts, and supporting multiple languages.
 # 
 # WTTR is highly customizable, allowing users to configure the output according to their preferences. 
 # The utility can be accessed through a web API or directly from the command line, making it convenient for developers and users who prefer command-line interfaces.
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
